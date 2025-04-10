71398a2fd37a61eafd4f50aa96c122ca13a3ebca
Venkateshkumar Selvarajan <Vselvar1@eu.trimblecorp.net>
2025-04-10 01:59:26 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { createToolDefinitions } from "./tools.js";
+import { setupRequestHandlers } from "./requestHandler.js";
+
+async function runServer() {
+  const server = new Server(
+    {
+      "name": "Trimble-UI-API-Automation",
+      "version": "0.2.7",
+    },
+    {
+      capabilities: {
+        resources: {},
+        tools: {},
+      },
+    }
+  );
+
+  // Create tool definitions
+  const TOOLS = createToolDefinitions();
+
+  // Setup request handlers
+  setupRequestHandlers(server, TOOLS);
+
+  // Create transport and connect
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+runServer().catch(console.error);
